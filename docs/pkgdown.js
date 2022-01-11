9c278172d5ea1e4db9bab33bb1f02e089d3bc6b8
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-01-11 10:33:07 +0100

    oopd

--- docs/pkgdown.js
+++ docs/pkgdown.js
@@ -6,7 +6,7 @@
 
     Toc.init({
       $nav: $("#toc"),
-      $scope: $("main h1, main h2, main h3, main h4, main h5, main h6")
+      $scope: $("main h2, main h3, main h4, main h5, main h6")
     });
 
     if ($('#toc').length) {
